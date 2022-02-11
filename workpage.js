9f3950117f31ab1b72c5dfaadc926bea7700c744
dot-code29 <99037684+dot-code29@users.noreply.github.com>
2022-02-11 17:13:31 -0500

    Add files via upload

---
+++ workpage.js
@@ -0,0 +1,34 @@
+function toggle1(){
+    var video_player_reel= document.querySelector(".video_player_reel")
+    video_player_reel.classList.toggle("active")
+ }
+
+ function toggle2(){
+    var video_player_shed= document.querySelector(".video_player_shed")
+    video_player_shed.classList.toggle("active")
+ }
+
+ function toggle3(){
+    var video_player_bkka= document.querySelector(".video_player_bkka")
+    video_player_bkka.classList.toggle("active")
+ }
+
+ function toggle4(){
+    var video_player_bkkb= document.querySelector(".video_player_bkkb")
+    video_player_bkkb.classList.toggle("active")
+ }
+
+ function toggle5(){
+    var video_player_bkkc= document.querySelector(".video_player_bkkc")
+    video_player_bkkc.classList.toggle("active")
+ }
+
+ function toggle6(){
+    var video_player_pbsc= document.querySelector(".video_player_pbsc")
+    video_player_pbsc.classList.toggle("active")
+ }
+
+ function toggle7(){
+    var video_player_arm= document.querySelector(".video_player_arm")
+    video_player_arm.classList.toggle("active")
+ }
